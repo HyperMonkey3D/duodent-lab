e77217eea9d08ead31d8656b400db318326336d1
HyperMonkey3D <luisfra.dev@gmail.com>
2023-06-06 14:57:50 -0600

    completed markup

--- components/sections/PriceList.tsx
+++ components/sections/PriceList.tsx
@@ -1,31 +1,37 @@
 import { works } from "@/app/utils/data"
 
 const PriceList = () => {
+
+
+
     return(
-        <section>
-            <div className="container mx-auto">
-                <table className="mx-auto ">
-                    <thead className="border-t border-r border-l border-duo-stroke  bg-duo-light-gray">
-                        <tr>
-                            <th>Producto</th>
-                            <th>Precio Unidad</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            works
-                            .map((item) => {
-                                const {index, product, price} = item
-                                return(
-                                    <tr key={index} className="border  border-duo-stroke">
-                                        <td >{product}</td>
-                                        <td>Q{(price)}</td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>   
+        <section className="border-y border-duo-stroke py-10">
+            <div className="container mx-auto ">
+                <div className="w-[1000px] mx-auto border border-duo-stroke rounded-lg">
+                    <table className="mx-auto w-full table-auto h-[700px] ">
+                        <thead className=" h-[50px] ">
+                            <tr >
+                                <th className="text-3xl text-left px-8 py-7 bg-duo-light-gray border-0 rounded-tl-lg">Producto</th>
+                                <th className="text-3xl text-left px-8 py-7 bg-duo-light-gray border-0 rounded-tr-lg">Precio Unidad</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                works
+                                .map((item) => {
+                                    const {index, product, price} = item
+                                    return(
+                                            <tr key={index} >
+                                                <td className="text-2xl text-left border-t border-duo-stroke px-8 py-7">{product}</td>
+                                                <td className="text-2xl text-left border-t border-duo-stroke px-8 py-7">Q{price}</td>
+                                            </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>  
+                </div>
+                 
             </div>
         </section>
     )
