bdf36bc233eaf3f383ee83588f9b1577886487f9
HyperMonkey3D <luisfra.dev@gmail.com>
2023-06-10 18:08:59 -0600

    minor changes

--- components/sections/HowWeHelp.tsx
+++ components/sections/HowWeHelp.tsx
@@ -1,22 +1,53 @@
+"use client"
 import Cards from "../common/Cards"
 import Image from "next/image"
 import lines from "../../public/stars.svg"
+import { useEffect, useState } from "react"
 
+const getCurrentSize = () => {
+    if(typeof window !== "undefined") {
+        return{
+            width: window.innerWidth,
+            height: window.innerHeight
+        }
+    }
+}
 
 const HowWeHelp = () => {
+
+    const [screnSize, setScreenSize] = useState(getCurrentSize())
+    const [large, setLarge] = useState(true)
+
+    useEffect(() => {
+        const updateSize = () => {
+            setScreenSize(getCurrentSize())
+        }
+        window.addEventListener("resize", updateSize)
+        if(typeof screnSize !== "undefined" && screnSize.width < 500) {
+            setLarge(false)
+        } else {
+            setLarge(true)
+        }
+        
+        console.log(screnSize)
+        return(()=> {
+            window.removeEventListener("resize", updateSize)
+        })
+    }, [screnSize])
+
     return(
         <section id="como-le-ayudamos" className="bg-duo-blue py-16 relative overflow-clip">
-            <div className=" absolute right-0 bottom-0  w-full h-full -z-1 ">
+            {large &&(<div className=" absolute right-0 bottom-0  w-full h-full -z-1">
                 <Image 
                     src={lines}
                     alt="lines"
-                    className="w-full scale-150 xl:scale-100"
+                    className="w-full "
                 />
-            </div>
+            </div>)}
            
 
         
-            <div className="container mx-auto z-10 ">
+            <div className="container mx-auto z-10">
                 <div className="w-full lg:w-[900px] xl:w-[1100px]  mx-auto ">
                     <div className="w-full px-10 lg:px-0 lg:w-3/4">
                         <h2 className="text-3xl md:text-5xl mb-9 text-white ">
