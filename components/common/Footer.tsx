2b5b3159ff4f6d4b19f33293a34c5a196bf52502
HyperMonkey3D <luisfra.dev@gmail.com>
2023-06-11 23:21:22 -0600

    responsive changes to footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -11,21 +11,21 @@ import whatsapp from "../../public/whatsapp.svg"
 const Footer = () => {
     return(
         <footer id="hablemos">
-            <div className="container mx-auto flex flex-wrap lg:flex justify-evenly py-[80px]">
-                <div className="flex flex-col justify-between">
-                    <div className="bg-duo-blue w-[260px] sm:w-[350] md:w-[450px] lg:w-[588px] h-[713px] border-0 rounded-lg ">
-                        <div >
+            <div className="container mx-auto flex flex-wrap lg:flex-nowrap justify-evenly py-[80px] bg-white">
+                <div className="flex flex-col justify-between bg-white">
+                    <div className="bg-duo-blue w-full lg:w-[500px]  xl:w-[588px] h-fit border-0 sm:rounded-lg ">
+                        <div className="w-full bg-white">
                             <Image 
                                 src={smile}
                                 alt="smile"
-                                className="border-0 rounded-t-lg"
+                                className="border-0 sm:rounded-t-lg w-full"
                             />
                         </div>
                         <div className="p-10 flex flex-col">
                             <p className="text-3xl text-white mb-8">
                                 Nos encataría endenter sus necesidades y ayudarle con sus pacientes. 
                             </p>
-                            <div className="flex">
+                            <div className="flex pb-5">
                                 <p className="text-2xl text-duo-light-blue-300 mr-6 font-bold">
                                     Hablemos
                                 </p>
@@ -33,12 +33,12 @@ const Footer = () => {
                                     src={arrowRight}
                                     alt="arrow right"
                                     height={18}
-                                    className="rotate-90 xl:rotate-0"
+                                    className="rotate-90 lg:rotate-0"
                                 />                            
                             </div>
                         </div>
                     </div>
-                    <div className="flex justify-evenly px-16">
+                    <div className="flex justify-evenly p-16 lg:pt-20 lg:pb-0 bg-white border-b border-duo-stroke lg:border-0">
                         <Image
                             src={facebook}
                             alt="facebook icon"
@@ -56,7 +56,7 @@ const Footer = () => {
                         />
                     </div>
                 </div>
-                <div className="w-[495px]">
+                <div className="bg-white w-4/5 lg:w-[400px] xl:w-[495px] mt-8 lg:mt-0">
                     <div className="w-full  h-full">
                         <div className="pb-6 ">
                             <h3 className="text-3xl text-duo-orange">Llámenos</h3>
